a9db8f5d32f0199074e25e57332f7874ffb6362d
OsamaAlmamri <osama1234887@gmail.com>
2020-09-12 00:46:49 +0300

    copy righ

--- resources/views/auth/login_1.blade.php
+++ resources/views/auth/login_1.blade.php
@@ -80,10 +80,9 @@
             </div>
         </div>
     </div>
-    <div class="copyrights text-center">
-        <p>Design by <a href="#" class="external">Adnan Almaqwali</a></p>
-        <!-- Please do not remove the backlink to us unless you support further theme's development at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)-->
-    </div>
+{{--    <div class="copyrights text-center">--}}
+{{--        <p>Design by <a href="#" class="external">Adnan Almaqwali</a></p>--}}
+{{--    </div>--}}
 </div>
 <!-- JavaScript files-->
 <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
